7ad3bb78cae8d4f3d542e86f8ec2e359ee99f10d
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-05-09 18:13:41 -0300

    feat: add link to product item

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface ProductItemProps {
     product: Prisma.ProductGetPayload<{
@@ -17,40 +21,42 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <div className="space-y-2 min-w-[150px] w-[150px]">
-            <div className="h-[150px] w-full relative">
-                <Image
-                    src={product.imageUrl}
-                    alt={product.name}
-                    fill
-                    className="object-cover rounded-lg shadow-md"
-                />
-                {product.discountPercentage && (
-                    <div className="absolute top-2 left-2 gap-[2px] bg-primary px-2 py-[2px] rounded-full text-white flex items-center">
-                        <ArrowDownIcon size={12} />
-                        <span className="text-xs font-semibold">{product.discountPercentage}%</span>
-                    </div>
-                )}
-            </div>
-
-            <div>
-                <h2 className="text-sm truncate">{product.name}</h2>
-                <div className="flex gap-1 items-center">
-                    <h3 className="font-semibold">
-                        {formatCurrency(calculateProductTotalPrice(product))}
-                    </h3>
-                    {product.discountPercentage > 0 && (
-                        <span className="line-through text-muted-foreground text-xs">
-                            {formatCurrency(Number(product.price))}
-                        </span>
+        <Link className="min-w-[150px] w-[150px]" href={`/products/${product.id}`}>
+            <div className="w-full space-y-2">
+                <div className="h-[150px] w-full relative">
+                    <Image
+                        src={product.imageUrl}
+                        alt={product.name}
+                        fill
+                        className="object-cover rounded-lg shadow-md"
+                    />
+                    {product.discountPercentage && (
+                        <div className="absolute top-2 left-2 gap-[2px] bg-primary px-2 py-[2px] rounded-full text-white flex items-center">
+                            <ArrowDownIcon size={12} />
+                            <span className="text-xs font-semibold">{product.discountPercentage}%</span>
+                        </div>
                     )}
                 </div>
 
-                <span className="text-muted-foreground text-xs truncate block">
-                    {product.restaurant.name}
-                </span>
+                <div>
+                    <h2 className="text-sm truncate">{product.name}</h2>
+                    <div className="flex gap-1 items-center">
+                        <h3 className="font-semibold">
+                            {formatCurrency(calculateProductTotalPrice(product))}
+                        </h3>
+                        {product.discountPercentage > 0 && (
+                            <span className="line-through text-muted-foreground text-xs">
+                                {formatCurrency(Number(product.price))}
+                            </span>
+                        )}
+                    </div>
+
+                    <span className="text-muted-foreground text-xs truncate block">
+                        {product.restaurant.name}
+                    </span>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 

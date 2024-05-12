48be6cab10ea3f2329289d7b59f77236f2b93a26
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-05-12 16:50:19 -0300

    feat: redirect to home page when click on header logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <div className="flex justify-between pt-6 px-5">
             <div className="relative h-[30px] w-[100px]">
-                <Image
-                    src="/logo.png"
-                    alt="FSW Foods"
-                    fill
-                    className="object-cover"
-                />
+                <Link href={'/'}>
+                    <Image
+                        src="/logo.png"
+                        alt="FSW Foods"
+                        fill
+                        className="object-cover"
+                    />
+                </Link>
             </div>
             <Button size={"icon"} variant={"outline"} className="bg-transparent border-none">
                 <MenuIcon />
